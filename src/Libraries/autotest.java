d677775648e73be9c3b2e5f4a7983fd5b97fdc7b
Suryakumarn <suryakumar9292@yahoo.com>
2017-08-14 16:11:18 +0530

    Signed-off-by: Suryakumarn <suryakumar9292@yahoo.com>

--- src/Libraries/autotest.java
+++ src/Libraries/autotest.java
@@ -1,13 +1,13 @@
 package Libraries;
-/*import autoitx4java.AutoItX;
+import autoitx4java.AutoItX;
 import com.jacob.com.LibraryLoader;
-import java.io.File;*/
+import java.io.File;
 
 public class autotest {
 	
 	public static void main(String argv[]) throws InterruptedException
 	{
-		/*String jacobDllVersionToUse = "C:\\Users\\admin\\Desktop\\FrameWork\\FrameWork\\lib\\jacob-1.18-x86.dll";
+		String jacobDllVersionToUse = "D:\\eclipse\\WorkSpace\\FrameWork\\lib\\jacob-1.18-x86.dll";
 		//File file = new File("lib", jacobDllVersionToUse);
 		
 		System.setProperty(LibraryLoader.JACOB_DLL_PATH, jacobDllVersionToUse);
@@ -25,7 +25,7 @@ public class autotest {
 		x.controlClick("Calculator", "", "133") ;
 		Thread.sleep(1000);
 		//Enter =
-		x.controlClick("Calculator", "", "121") ;*/
+		x.controlClick("Calculator", "", "121") ;
 		
 		
 	}
